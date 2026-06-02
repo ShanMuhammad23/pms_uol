cbfb443ccfbcaeac3a6ca31567bf9f852515318a
Shan Muhammad <shanisitedeveloper@gmail.com>
2026-06-02 15:18:20 +0500

    Refactor Sidebar component to use new icons, improve layout structure, and enhance user profile display. Update dashboard page to utilize server session for authentication and streamline redirect logic.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,18 @@
-'use client'
-import { redirect } from 'next/navigation'
-import { useSession } from 'next-auth/react'
-const page = async () => {  
-  const { data: session } = useSession()
-  if (!session) {
-    redirect('/')
-  }
-  return (
-    <div className='text-text-primary'>
-      <h1 className='text-2xl font-bold'>Welcome, {session?.user?.name}</h1>
-    </div>
-  )
-}
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/auth";
 
-export default page
+const page = async () => {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/");
+  }
+
+  return (
+    <div className="text-text-primary">
+      <h1 className="text-2xl font-bold">Welcome, {session.user?.name}</h1>
+    </div>
+  );
+};
+
+export default page;
